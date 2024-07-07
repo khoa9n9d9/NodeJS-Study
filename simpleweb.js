7888523ede6773a83b3950a71ecb0e9f0eb1edf0
aohkkhoa <ndk1031999@gmail.com>
2024-07-07 15:33:28 +0700

    routing

--- simpleweb.js
+++ simpleweb.js
@@ -2,7 +2,10 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    res.end('hê lô from web')
+    if(req.url === '/' || req.url === 'khoa')
+        res.end('hê lô from web')
+    else
+        res.end('cái này khác') 
 })
 
 server.listen(8088, 'localhost', () =>{
