0d0275612961b74d08b2522d1e2609a6565c326e
aohkkhoa <ndk1031999@gmail.com>
2024-07-07 16:04:54 +0700

    simple api

--- simpleweb.js
+++ simpleweb.js
@@ -2,11 +2,18 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    if(req.url === '/' || req.url === 'khoa')
+    if(req.url === '/' || req.url === '/khoa')
         res.end('hê lô from web')
+    else if (req.url === '/api'){
+        fs.readFile('./data.json', 'utf-8', (err, data) =>{
+            const productData = JSON.parse(data);
+            console.log(productData)
+        })
+        res.end('API');
+    }
     else
-        res.end('cái này khác') 
-})
+        res.end('cái này khác')  
+    })
 
 server.listen(8088, 'localhost', () =>{
     console.log('listening to request ==s on port 8088')
